d69f077ab7bad189b4e2fbd7d0ae8d487c85c5ff
Nataly-Naf <nat.spivak84@gmail.com>
2024-03-03 16:44:27 +0000

    Deploying to gh-pages from @ Nataly-Naf/Medicine-Delivery-app@fbb78d953d31275a9f9717070d1b0d2d65d55a6a 🚀

--- static/js/681.65e314bb.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[681],{9681:function(e,t,r){r.r(t),r.d(t,{default:function(){return a}});var n=r(184);function a(){return(0,n.jsx)("div",{children:"Basket page"})}}}]);
-//# sourceMappingURL=681.65e314bb.chunk.js.map
